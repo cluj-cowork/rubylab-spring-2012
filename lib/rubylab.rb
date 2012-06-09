fe2771007d1009bfd200afe5d88f99e2178255e1
Stas SUȘCOV <stas@net.utcluj.ro>
2012-06-09 13:24:29 +0300

    Acum putem sa ne conectam la Campfire si sa trimitem mesaje.

--- lib/rubylab.rb
+++ lib/rubylab.rb
@@ -1,7 +1,9 @@
-require "rubylab/version"
+require 'rubylab/version'
 
 # "Prefixul claselor/metodelor noastre, aka namespace
 module Rubylab
+
+require 'tinder'
   
   # Clasa noastra de conectare la campfire
   class Campfire
@@ -13,20 +15,32 @@ module Rubylab
         afiseaza_camerele
     end
     
-    # Ceara utilizatorului parola de la campfire
+    # Cere utilizatorului parola de la campfire
     def citeste_parola
         puts 'Tasteaza parola, aka tokenul tau Campfire:'
         @parola = gets.strip
     end
     
+    # Cere utilizatorului un mesaj
+    # @return [String], mesajul citit
+    def citeste_mesajul
+        puts 'Scrie un mesaj:'
+        return gets.strip
+    end
+    
     # Interogheaza API-ul si listeaza camerele la care avem access
+    # @param [String] mesaj, ce sa se scrie la camera noastra
     def afiseaza_camerele
         campfire = Tinder::Campfire.new('rubytribe2', :token => @parola)
         puts "Ai #{campfire.rooms.count} camere:"
-        puts campfire.rooms.inspect
-        #campfire.rooms.each do |camera|
-        #    puts "\t #{camera} \n"
-        #end
+        
+        campfire.rooms.each do |camera|
+            puts "\t #{camera.name} \n"
+            if (mesaj = citeste_mesajul) != ''
+                camera.speak(mesaj)
+                puts "\t\t Mesaj trimis.\n"
+            end
+        end
     end
   
   end # class
